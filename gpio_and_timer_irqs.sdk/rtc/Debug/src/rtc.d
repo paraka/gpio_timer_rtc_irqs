a445164e921db34afffa7e126a42bfd6bd6d3523
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-12-05 18:49:16 +0100

    gpio timer irqs: add project files

---
+++ gpio_and_timer_irqs.sdk/rtc/Debug/src/rtc.d
@@ -0,0 +1,58 @@
+src/rtc.o src/rtc.d: ../src/rtc.c \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xtmrctr.h \
+ ../../rtc_bsp/ps7_cortexa9_0/include/xtmrctr_l.h
+
+../../rtc_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xtmrctr.h:
+
+../../rtc_bsp/ps7_cortexa9_0/include/xtmrctr_l.h:
